e433e3fa5b1ba4979f2d3553cbfd690d0b48ee5e
ILHAM KHAN <khanilham12@gmail.com>
2026-06-21 08:10:48 +0530

    Connect frontend to deployed API

--- frontend/script.js
+++ frontend/script.js
@@ -48,7 +48,7 @@ async function predictRisk() {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:8000/predict",
+            "https://heart-disease-prediction-system-nlpn.onrender.com/predict",
             {
                 method: "POST",
 
